34bd788f2d2212873865cb05236a2f6974d61991
Diwakar Dixit <diwakardixit98@gmail.com>
2023-01-31 19:01:50 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Label } from "semantic-ui-react";
+import "./Components/CounterSemanticUI.css";
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [buttonClass, setButtonClass] = useState("primary");
+
+  const DecrementFn = () => {
+    if (count <= 0) {
+      setCount(0);
+      setButtonClass("secondary");
+    } else {
+      setCount(count - 1);
+      setButtonClass("primary");
+    }
+  };
+
+  // Increment Function
+  const IncrementFn = () => {
+    setCount(count + 1);
+    setButtonClass("primary");
+  };
+
+  let socialcount = 0;
+  const social = () => {
+    if (socialcount % 5 === 0 || socialcount === 0) {
+    }
+    socialcount += 1;
+  };
+  console.clear();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h4>A Counter Application Using useState()</h4>
+      <div className="Card">
+        <Label className="Count" size="huge">
+          Count{" "}
+        </Label>
+        <Label className="CountNumber" size="huge">
+          {count}
+        </Label>
+        <Button.Group>
+          <Button className="increment" onClick={IncrementFn}>
+            {" "}
+            Increment{" "}
+          </Button>
+          <Button className={buttonClass} onClick={DecrementFn}>
+            Decrement{" "}
+          </Button>
+        </Button.Group>
+      </div>
+      <p onMouseOver={social} className="credit">
+        <span></span>
+      </p>
     </div>
   );
 }
 
-export default App;
+export default Counter;
---
+++ src/Components/CounterDisplay.js
@@ -0,0 +1,8 @@
+function CounterDisplay({ count }) {
+  return (
+    <p className="counter-display">
+      Count <br /> {count}
+    </p>
+  );
+}
+export default CounterDisplay;
